a0abafc192b737b088959b832ced5cb5ba2e887b
kunxin-chor <chorkunxin@yahoo.com>
2020-09-09 06:10:28 +0000

    Migrateed

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Employee(models.Model):
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    employee_number = models.PositiveIntegerField(blank=False, unique=True)
+    first_name = models.CharField(blank=False, max_length=255)
+    last_name = models.CharField(blank=False, max_length=255)
+    salary = models.PositiveIntegerField(blank=False)
+    months_employed = models.PositiveSmallIntegerField(blank=False)
+    leave = models.PositiveSmallIntegerField(blank=False, default=14)
+    department = models.ForeignKey('Department', on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return f"{self.employee_number} - {self.first_name} {self.last_name}"
+
+
+class Department(models.Model):
+    name = models.CharField(blank=False, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class MedicalLeave(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    start_date = models.DateField(blank=False)
+    end_date = models.DateField(blank=False)
+
+    def __str__(self):
+        return f"Medicial leave ({self.start_date} to {self.end_date}) for " \
+               f"{self.employee.first_name}"
--- employees/tests.py
+++ employees/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+from .models import Employee, Department, MedicalLeave
 
 # Create your tests here.
+
+
+class EmployeeModelTestCase(TestCase):
+
+    def test_employee_has_default_14_days_leave(self):
+        # 1. create the model instances required for the relationships
+        user = User.objects.create_user(username="Ah Kow")
+        department = Department.objects.create_department(name="Accounting")
+
+        # 2. create the Employee model instance
+        employee = Employee()
+        employee.first_name = "Ah Kow"
+        employee.last_name = "Tan"
+        employee.salary = 1200
+        employee.months_employed = 3
+        employee.user = user
+        employee.owner = department
+        employee.save()
+
+        # 3. test that save has been done correctly
+        # if done correct, an id will be assigned to the employee object
+        self.assertTrue(employee.id > 0)
+
+        # 4. retrieve the employee from the database
+        test_employee = get_object_or_404(Employee, employee.id)
+
+        # 5. test if the employee's default number of leave is 14
+        self.assertEqual(test_employee.leave, 14)
